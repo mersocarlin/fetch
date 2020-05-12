549aadb7f4406816e1671ab215ec3a525b21da7a
mersocarlin <mersocarlin@gmail.com>
2020-05-12 20:41:15 +0100

    Setup tests

---
+++ src/fetch.spec.ts
@@ -0,0 +1,72 @@
+import {
+  GatewayTimeoutError,
+  InternalServerError,
+} from '@mersocarlin/api-error'
+
+import { startServer } from './fixtures/fakeServer'
+import fetch from './fetch'
+
+describe('fetch', () => {
+  let server: any
+
+  beforeAll(async () => {
+    server = await startServer()
+  })
+
+  afterAll(done => {
+    if (server) {
+      server.close(done)
+    }
+  })
+
+  it('should make get request', async () => {
+    const { body, statusCode } = await fetch('http://localhost:3000/api/users')
+
+    expect(statusCode).toEqual(200)
+    expect(Array.isArray(body)).toBeTruthy()
+  })
+
+  it('should handle text/html content-type', async () => {
+    const { body, statusCode } = await fetch(
+      'http://localhost:3000/api/users-html',
+      {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }
+    )
+
+    expect(statusCode).toEqual(200)
+    expect(typeof body).toEqual('string')
+  })
+
+  it('should handle text/plain content-type', async () => {
+    const { body, statusCode } = await fetch(
+      'http://localhost:3000/api/users-plain',
+      {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }
+    )
+
+    expect(statusCode).toEqual(200)
+    expect(typeof body).toEqual('string')
+  })
+
+  it('should handle timeout', () => {
+    expect(fetch('http://localhost:3000/api/users/timeout')).rejects.toEqual(
+      new GatewayTimeoutError(
+        'request to http://localhost:3000/api/users/timeout failed, reason: connect ECONNREFUSED 127.0.0.1:3000'
+      )
+    )
+  })
+
+  it('should handle error', () => {
+    expect(fetch('http://localhost:3000/api/users/error')).rejects.toEqual(
+      new InternalServerError(
+        'request to http://localhost:3000/api/users/error failed, reason: connect ECONNREFUSED 127.0.0.1:3000'
+      )
+    )
+  })
+})
---
+++ src/fixtures/fakeServer.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+
+const users = Array.from({ length: 10 }).map((_, index) => ({
+  id: index,
+  name: `User ${index}`,
+}))
+
+export const startServer = ({ port = process.env.PORT } = {}) => {
+  const app = express()
+  app.use(bodyParser.json())
+
+  const router = express.Router()
+  router.get('/users', (_req: Request, res: Response) => res.send(users))
+  router.get('/users-html', (_req: Request, res: Response) => {
+    res.set('Content-Type', 'text/html').send(users)
+  })
+  router.get('/users-plain', (_req: Request, res: Response) => {
+    res.set('Content-Type', 'text/plain').send(users)
+  })
+  router.get('/users/timeout', (_req: Request, _res: Response) => {
+    _req.setTimeout(100)
+    _res.setTimeout(100)
+  })
+  router.get('/users/error', () => {
+    throw new Error('Something went wrong!')
+  })
+
+  app.use('/api', router)
+
+  return new Promise(resolve => {
+    const server = app.listen(port, () => {
+      resolve(server)
+    })
+  })
+}
